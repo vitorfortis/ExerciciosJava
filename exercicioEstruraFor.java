a6fdff0b74bf8ea52e65bef559bfa88a9e050add
Vitor Alexandre <85693835+vitorfortis@users.noreply.github.com>
2022-01-24 21:35:31 -0300

    "Exercício Estrutura FOR 01"
    Programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas, 
    começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor.

---
+++ exercicioEstruraFor.java
@@ -0,0 +1,22 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class exercicioEstruraFor {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i<=n; i++) {
+			
+			int primeiro = i;
+			int segundo = i*i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo,terceiro);
+		}
+        sc.close();
+	}
+
+}
